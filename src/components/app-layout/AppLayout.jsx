82ae69648a964d538b990f4e60c3b4c80676a881
mounirabcire <mounirwebdevcode@gmail.com>
2024-05-29 10:50:22 +0100

    updated

--- src/components/app-layout/AppLayout.jsx
+++ src/components/app-layout/AppLayout.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import { Outlet, useNavigation } from "react-router-dom";
+
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/Footer";
 import Loader from "../../loader/Loader";
 
 function AppLayout() {
+    // Hooks
     const navigation = useNavigation();
+    // Keep track of wether the borowser has painted or not
+    const [hasPainted, setHasPainted] = useState();
+
+    useEffect(() => {
+        // Request animation frame to check if the browser has painted
+        const rafId = requestAnimationFrame(() => {
+            setHasPainted(true);
+        });
+
+        // Clean up the requestAnimationFrame callback
+        return () => cancelAnimationFrame(rafId);
+    }, []);
 
     return (
         <>
             <Navbar />
-            {navigation.state === "loading" ? (
+            {navigation.state === "loading" || !hasPainted ? (
                 <Loader />
             ) : (
                 <>
