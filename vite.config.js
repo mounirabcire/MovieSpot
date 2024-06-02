54133906798818a9a13453acbb4c03702b2b0d05
mounirabcire <mounirwebdevcode@gmail.com>
2024-06-03 00:15:25 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
-  base: '/vite-react-deploy/'
+    plugins: [react(), eslint()],
+    base: "/MovieSpot/",
 });
